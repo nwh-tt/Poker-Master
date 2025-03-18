dce62d01595b90e673146950304256b8af535475
Ned Whittleton <nedwhitt2@gmail.com>
2025-03-18 16:39:43 -0400

    First commit

--- Poker MasterTests/GameManagerTests.swift
+++ Poker MasterTests/GameManagerTests.swift
@@ -20,15 +20,6 @@ import XCTest
 final class GameManagerTests: XCTestCase {
     let gameManager = GameManager()
     
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
     func testGameManagerInitialization() {
             let gameManager = GameManager()
             
