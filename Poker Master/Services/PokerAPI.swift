8d7e7211f3b863563dbef5275400480de9ecf7c3
Ned Whittleton <nedwhitt2@gmail.com>
2025-08-06 14:57:29 -0400

    Commit before messing something up probably

---
+++ Poker Master/Services/PokerAPI.swift
@@ -0,0 +1,8 @@
+//
+//  PokerAPI.swift
+//  Poker Master
+//
+//  Created by Ned Whittleton on 8/6/25.
+//
+
+import Foundation
---
+++ Poker Master/State/NavigationState.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationState.swift
+//  Poker Master
+//
+//  Created by Ned Whittleton on 8/4/25.
+//
+
+import SwiftUI
+
+struct NavigationState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationState()
+}
